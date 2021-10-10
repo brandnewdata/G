542bec70eb75a7ce804afb5bcf58d09d06158f35
fvwstudio <brandnewdata@gmail.com>
2021-10-10 23:17:57 +0900

    Docs :  @ ID3D12Resource, @ DescHeap 설명 추가 @ 더블 버퍼링 @ Swap Chain 설명추가

--- DX12Game/Engine/SwapChain.h
+++ DX12Game/Engine/SwapChain.h
@@ -1,6 +1,19 @@
 #pragma once
 #include "pch.h"
 
+// @ ID3D12Resource, @ DescHeap 렌더 타겟이든지 텍스쳐든지 GPU에서 사용할 메모리덩어리(Resource)에 대한 스펙을 묘사한 구조체입니다.
+// DX11 View == DX12 Descriptor
+// DX11의 RTV(RenderTargetVeiw), DSV(DepthStencilView), CBV(ConstantBufferView), UAV(UnorderedAccessView) 전부
+// DX12에서는 DescHeap에 입력한 스펙대로 사용할 메모리를 배정받음.
+// 어쩌면 이 부분은 덜 복잡해진 것 같기도 함.
+//
+// @ 더블 버퍼링과 @ Swap Chain
+// 렌더링용 버퍼는 n개 존재 할 수있습니다. 하지만 성능상의 이유로 보통 2개만 써서 더블 버퍼링을 합니다.
+// @ Back Buffer  : DescHeap에 설정된 스펙에 따라서 렌더링중인 버퍼.
+// @ Front Buffer : 출력 처리에 사용중인 버퍼 그래픽 카드에서만 사용한다. 버퍼의 내용을 그대로 모니터에 출력한다.
+// @ Presentation : Back Buffer에 렌더링이 끝나면 Front Buffer에서 Back Buffer로 바꿉니다.
+// 한마디로 인데스 0번 1번이 있다면 서로 계속 돌아가면서 모니터에 출력 합니다.
+
 class SwapChain
 {
 	// 초기화
